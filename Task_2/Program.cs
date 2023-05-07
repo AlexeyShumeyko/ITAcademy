7d9917c6d4ea34e4d3db7f9b5188510238e2bef4
AlexeyShumeyko <schumeiko.alexei@yandex.by>
2023-05-08 00:50:09 +0300

    com

--- Task_2/Program.cs
+++ Task_2/Program.cs
@@ -6,6 +6,6 @@ Console.WriteLine(example.DuplicateThirdSimbol());
 int[,] exampleArray = { { 1, 2, 3, 5, 84 }, { 4, 5, 6, 34, 84 } };
 Console.WriteLine(String.Join(" ", exampleArray.GhangeArrayDimension()));
 
-// Task 2, Part C
+// Task 2, Part C,
 int[] inArray = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
 int[,] outArray = inArray.GhangeArrayDimension(3);
