8e84de3d877d8b3a2b7dbb38304929fca7c5b6e8
AlexeyShumeyko <schumeiko.alexei@yandex.by>
2023-05-08 10:17:27 +0300

    Completed homework

--- HomeWork_2/BaseEntities/Car.cs
+++
@@ -1,9 +0,0 @@
-﻿public abstract class Car : Vehical
-{
-    public int WeelsNumber { get; set; }
-    public override string ToString()
-    {
-        return base.ToString() + $"\nWeels Number: {WeelsNumber}";
-    }
-    public abstract void Charge();
-}
--- HomeWork_2/BaseEntities/FlyingVehical.cs
+++
@@ -1,9 +0,0 @@
-﻿public abstract class FlyingVehical : Vehical
-{
-    public double MaxLiftingHeight { get; set; }
-    public abstract void GetMechanismOfFly();
-    public override string ToString()
-    {
-        return base.ToString() + $"\nMaxLiftingHeight: {MaxLiftingHeight}";
-    }
-}
