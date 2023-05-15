768e63c8cbfb1f6fbced7efe64ee520b408177bf
AlexeyShumeyko <schumeiko.alexei@yandex.by>
2023-05-15 19:45:57 +0300

    Completed homework3

---
+++ HomeWork_3/CurrencyExchangeRequest.cs
@@ -0,0 +1,45 @@
+﻿public class CurrencyExchangeRequest
+{
+    private Currency From { get; set; }
+    private Currency To { get; set; }
+    private decimal Amount { get; set; }
+    private Dictionary<int, double> ValueCollectionMemory { get; set; }
+
+    public CurrencyExchangeRequest(Currency From, Currency To, decimal Amount, Dictionary<int, double> valueCollectionMemory)
+    {
+        this.From = From;
+        this.To = To;
+        this.Amount = Amount;
+        ValueCollectionMemory = valueCollectionMemory;
+    }
+
+    public decimal ConvertCurrency(CurrencyExchangeRequest request)
+    {
+        var rateCurrency = СheckMemoryCollection(From, To);
+
+        return (decimal)rateCurrency * request.Amount;
+    }
+
+    private double СheckMemoryCollection(Currency From, Currency To)
+    {
+        int KeyСurrency = ((int)From) * ((int)To) * 333;
+
+        if (ValueCollectionMemory.ContainsKey(KeyСurrency))
+        {
+            return ValueCollectionMemory[KeyСurrency];
+        }
+        else
+        {
+            ValueCollectionMemory.Add(KeyСurrency, GetRandomValue());
+
+            return ValueCollectionMemory[KeyСurrency];
+        }
+    }
+
+    private double GetRandomValue()
+    {
+        var randomValue = new Random();
+
+        return randomValue.NextDouble() * 3;
+    }
+}
---
+++ HomeWork_3/Program.cs
@@ -0,0 +1,24 @@
+﻿List<decimal> conversionMoney = new List<decimal>();
+Dictionary<int, double> valueCollectionMemory = new Dictionary<int, double>();
+
+var request = new CurrencyExchangeRequest(Currency.USD, Currency.RUB, 30.60M, valueCollectionMemory);
+conversionMoney.Add(request.ConvertCurrency(request));
+
+var request2 = new CurrencyExchangeRequest(Currency.BYN, Currency.RUB, 679, valueCollectionMemory);
+conversionMoney.Add(request2.ConvertCurrency(request2));
+
+var request3 = new CurrencyExchangeRequest(Currency.USD, Currency.RUB, 30.60M, valueCollectionMemory);
+conversionMoney.Add(request3.ConvertCurrency(request3));
+
+var request4 = new CurrencyExchangeRequest(Currency.BYN, Currency.RUB, 679, valueCollectionMemory);
+conversionMoney.Add(request4.ConvertCurrency(request4));
+
+Console.WriteLine(string.Join(" ", conversionMoney));
+
+public enum Currency
+{
+    BYN = 21,
+    RUB = 45,
+    EUR = 34,
+    USD = 38
+}
