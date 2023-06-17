cf9e1a27c0735b433aa50f2166b9e5ea58bbeee2
AlexeyShumeyko <schumeiko.alexei@yandex.by>
2023-06-18 00:54:03 +0300

    Completed homework 8

---
+++ HomeWork_6/FileHandler.cs
@@ -0,0 +1,50 @@
+﻿public class FileHandler
+{
+    private int _totalCountWord = 0;
+    private object _lock = new object();
+
+    public Task WordCountTextThread (string pathToDirectory)
+    {
+        var fileList = Directory.GetFiles(pathToDirectory);
+        if (fileList.Length == 0)
+        {
+            Console.WriteLine("File not found");
+            return Task.FromResult(0);
+        }
+
+        Task[] tasks = new Task[fileList.Length];
+
+        int indexFile = 0;
+
+        for (int i = 0; i < tasks.Length; i++)
+        {
+            tasks[i] = Task.Run(() => WordCountText(fileList[indexFile++]));
+        }
+
+        Task.WaitAll(tasks);
+
+        return Task.CompletedTask;
+    }
+
+    private void WordCountText(string path)
+    {
+        string allText;
+
+        var fileInfo = new FileInfo(path);
+
+        using (StreamReader sr = new StreamReader(path))
+        {
+            allText = sr.ReadToEnd();
+        }
+
+        int countWords = allText.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).Length;
+
+        lock (_lock)
+        {
+            Console.WriteLine($"\nFile {fileInfo.Name} has finished counting words : {countWords}");
+            _totalCountWord += countWords;
+            Console.WriteLine($"The total sum of words at the moment is : {_totalCountWord}\n");
+        }
+    }
+
+}
---
+++ HomeWork_6/Program.cs
@@ -0,0 +1,31 @@
+﻿string pathToDirectory = InputUser();
+
+var test = new FileHandler();
+test.WordCountTextThread(pathToDirectory);
+
+static string InputUser()
+{
+    bool state = true;
+    string pathToDirectory;
+
+    do
+    {
+        Console.WriteLine("Specify the path to the directory");
+        pathToDirectory = Console.ReadLine();
+
+        if (Directory.Exists(pathToDirectory))
+        {
+            state = false;
+        }
+        else
+        {
+            Console.WriteLine("Directory not found or incorrect directory path");
+            Console.WriteLine("Press key to continue");
+            Console.ReadLine();
+            Console.Clear();
+        }
+    }
+    while (state);
+
+    return pathToDirectory;
+}
