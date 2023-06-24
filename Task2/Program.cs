358aa0daaeabdafe6c11946bd7316835acc3f244
AlexeyShumeyko <schumeiko.alexei@yandex.by>
2023-06-24 15:23:09 +0300

    Всё плохо :)

---
+++ Task2/Program.cs
@@ -0,0 +1,4 @@
+﻿int[] numbers = { 3, 12, 4, 10 };
+var orderedNumbers = numbers.OrderBy(n => n).ToArray();
+var count = Math.Ceiling((decimal)orderedNumbers.Length / 2);
+var res = orderedNumbers.Skip((int)count).Select(x => x*x);
---
+++ Task4/Program.cs
@@ -0,0 +1,30 @@
+﻿public static class MyMath
+{
+
+
+    public static int Factorial(int number)
+    {
+        if (number == 1) return 1;
+
+        try
+        {
+            return number * Factorial(number - 1);
+        }
+        catch (OverflowException ex)
+        {
+            Console.WriteLine(ex);
+            return -1;
+        }
+    }
+
+    public static int Exponentiation(int number, int degree)
+    {
+        var result = 1;
+        for (int i = 0; i < degree; i++)
+        {
+            result *= number;
+        }
+        return result;
+    }
+
+}
